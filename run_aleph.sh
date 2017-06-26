bc205fff986b7669b5c5f4729438ab398b665274
brenowcarvalho <brenowcarvalho@gmail.com>
2017-06-25 23:56:11 -0300

    adds code to run Aleph

---
+++ run_aleph.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -gt "0" ]; then
+	direct=$1
+	prefix=$2
+	#echo ">$direct"
+	#echo ">$prefix"
+	echo ":- consult(aleph).
+:- read_all('$direct/$prefix').
+:- induce.
+:- halt." | yap
+else
+	>&2 echo "Usage: $0 dir file_prefix"
+fi
