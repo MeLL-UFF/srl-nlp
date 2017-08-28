3066b3c48c37f88ba35dc749c3eb52bfbea8201b
brenowcarvalho <brenowcarvalho@gmail.com>
2017-08-28 19:44:16 -0300

    minor typos

--- parse_results.py
+++ parse_results.py
@@ -6,30 +6,14 @@ from os            import path, walk as walkdir
 from sys           import argv
 from json          import load,dump
 from regex         import compile
-from learner       import Aleph, run_tree, config_logger
+from learner       import Aleph, ProbLog, run_tree
 from logger_config import config_logger, add_logger_args
 import logging
 import argparse
 
 logger = logging.getLogger(__name__)
 
-def _runAleph_out_parser(dir, file_list, prefix = None, logger = logging.getLogger(__name__), d = {}):
-    file_name = Aleph._find_file(prefix, None, file_list, logger)
-    if file_name:
-        complete_file_name = path.join(dir, file_name)
-        logger.info('File name: %s', complete_file_name)
-        d.update({complete_file_name: Aleph.process_out(complete_file_name)})
-
-def parse_args(argv = argv, add_logger_args = lambda x: None):
-    parser = argparse.ArgumentParser(description = 'Parses the ourput of an experiment tree (Aleph only right now)')
-    parser.add_argument('dir_path', help = 'the path of the experiments')
-    parser.add_argument('-p', '--file_prefix', help = 'prefix of the experiment output files')
-    parser.add_argument('-o', '--output_file', help = 'file to store results (if None: print to stdout)')
-    add_logger_args(parser)
-    args = parser.parse_args(argv[1:])
-    return args
-
-class DictObj:
+class ExpObj:
     '''
     Object representing an experiment output
     '''
@@ -38,7 +22,7 @@ class DictObj:
         for key in entries.keys():
             tokens = map(lambda x: self._normalize_token(x, repl), key.split(sep))
             if len(tokens) > 1:
-                new_entries[tokens[0]] = new_entries.get(tokens[0],DictObj())
+                new_entries[tokens[0]] = new_entries.get(tokens[0],ExpObj())
                 new_entries[tokens[0]]._update(self._unfold(tokens[1:],entries[key]))
             else:
                 new_entries[tokens[0]] = entries[key]
@@ -47,7 +31,7 @@ class DictObj:
     def _update(self, other):
         for key in other.__dict__:
             if key in self.__dict__:
-                if isinstance(self.__dict__[key], DictObj):
+                if isinstance(self.__dict__[key], ExpObj):
                     self.__dict__[key]._update(other.__dict__[key])
             else:
                  self.__dict__[key] = other.__dict__[key]
@@ -55,12 +39,12 @@ class DictObj:
 
     def _unfold(self, list_tokens, val):
         if len(list_tokens) > 1:
-            return DictObj(**{list_tokens[0]: self._unfold(list_tokens[1:], val)})
+            return ExpObj(**{list_tokens[0]: self._unfold(list_tokens[1:], val)})
         else:
-            if isinstance(val, dict) or isinstance(DictObj):
-                return DictObj(**{list_tokens[0]: DictObj(**val)})
+            if isinstance(val, dict) or isinstance(ExpObj):
+                return ExpObj(**{list_tokens[0]: ExpObj(**val)})
             else:
-                return DictObj(**{list_tokens[0]: val})
+                return ExpObj(**{list_tokens[0]: val})
 
     def _normalize_token(self, token, repl, forbiden = '+-*, .'):
         for i in forbiden:
@@ -69,8 +53,30 @@ class DictObj:
 
 def json_to_object(json):
     with open(json, 'r') as f:
-        return DictObj(**load(f))
+        return ExpObj(**load(f))
 
+def _runAleph_out_parser(dir, file_list, prefix = None, logger = logging.getLogger(__name__), d = {}):
+    file_name = Aleph._find_file(prefix, None, file_list, logger)
+    if file_name:
+        complete_file_name = path.join(dir, file_name)
+        logger.info('File name: %s', complete_file_name)
+        d.update({complete_file_name: Aleph.process_out(complete_file_name)})
+
+def _runProbLog_out_parser(dir, file_list, prefix = None, logger = logging.getLogger(__name__), d = {}):
+    file_name = ProbLog._find_file(prefix, None, file_list, logger)
+    if file_name:
+        complete_file_name = path.join(dir, file_name)
+        logger.info('File name: %s', complete_file_name)
+        d.update({complete_file_name: ProbLog.process_out(complete_file_name)})
+
+def parse_args(argv = argv, add_logger_args = lambda x: None):
+    parser = argparse.ArgumentParser(description = 'Parses the ourput of an experiment tree (Aleph only right now)')
+    parser.add_argument('dir_path', help = 'the path of the experiments')
+    parser.add_argument('-p', '--file_prefix', help = 'prefix of the experiment files')
+    parser.add_argument('-o', '--output_file', help = 'file to store results (if None: print to stdout)')
+    add_logger_args(parser)
+    args = parser.parse_args(argv[1:])
+    return args
 
 def main(argv):
     args = parse_args(argv, add_logger_args)
--- write_experiments.py
+++ write_experiments.py
@@ -27,154 +27,191 @@ class ProcessFile(object):
         out_file.write('\n'.join(self.lines))
         #self.logger.info('Wrote')
 
-
-class ProcessNegatives(ProcessFile):
-    def __init__(self, source_file_path, *args, **kargs):
-        '''
-        Process the negative files (*.n)
-        Params:
-            patttern: regex pattern to parse a line
-            target:   target predicate to look
-        '''
-        super(ProcessNegatives,self).__init__(source_file_path, *args, **kargs)
-        target = kargs.get('target', 'answer')
-        pat = compile(kargs.get('pattern', '%s\s*\(\s*(\d+)\s*,\s*[^\)]+\s*\)\s*.\s*' % target))
-        self.logger.info('Reading from "%s"',source_file_path)
-        with open(source_file_path, 'r') as source:
-            dic = {}
-            for line in source:
-                m = pat.match(line)
-                if m:
-                    l = dic.get(m.group(1),[])
-                    l.append(line.strip())
-                    if dic.get('random', False):
-                        shuffle(l)
-                    dic[m.group(1)] = l
-                    self.logger.debug('Negatives %s...%s',str(l)[:50],str(l)[-20:])
-                else:
-                    self.logger.warning('Skipping line: "%s"', line.strip())
-        self.logger.info('Read "%s"',source_file_path)
-        lim = kargs.get('max', None)
-        for key in dic:
-            if lim == None:
-                self.lines.extend(dic[key])
-            else:
-                self.lines.extend(dic[key][:lim])
-        self.logger.info('Storing negatives')
-
-
-class ProcessBase(ProcessFile):
-    def __init__(self, source_file_path, *args, **kargs):
-        '''
-        Process the negative files (*.n)
-        Params:
-            patttern: regex pattern to parse a line
-            target:   target predicate to look
-        '''
-        super(ProcessBase,self).__init__(source_file_path, *args, **kargs)
-        
-        self.logger.info('Reading from "%s"',source_file_path)
-        sources = kargs.get('sources',[])
-        sources = map(lambda x: x.lower(), sources)
-        with open(source_file_path, 'r') as source:
-            boxer_mode_lines = []
-            boxer_det_lines  = []
-            dtree_mode_lines = []
-            dtree_det_lines  = []
-            header_lines = []
-            footer_lines = []
-            setting_dict = {}
-            boxer_flag = False
-            deptree_flag = False
-            header_flag = True
-            find_boxer = compile(r'%\s*((?:b|B)oxer)\s*.*')
-            find_dtree = compile(r'%\s*((?:d|D)ep(?:t|T)ree)\s*.*')
-            find_pred  = compile('^\s*:-\s*([^\(]+)\s*\(.*')
-            find_set  =  compile('^\s*:-\s*set\s*\((\w+)\s*,\s*(\w*)\s*\)\s*\.\s*')
-            for line in source:
-                pred_match = find_pred.match(line)
-                if find_boxer.match(line):
-                    boxer_flag = True
-                    deptree_flag = False
-                    header_flag = False
-                    self.logger.debug('Reading Boxer info from base')
-                elif find_dtree.match(line):
-                    boxer_flag = False
-                    deptree_flag = True
-                    header_flag = False
-                    self.logger.debug('Reading DepTree info from base')
-                elif pred_match:
-                    self.logger.debug('Line is a predicate: %s', line.strip())
-                    if pred_match.group(1) == 'set':
-                        #handle_set
-                        term, value = find_set.match(line).groups()
-                        self.logger.debug('Setting %s:%s', term, value)
-                        setting_dict[term] = value
+class Aleph:
+    class ProcessNegatives(ProcessFile):
+        def __init__(self, source_file_path, *args, **kargs):
+            '''
+            Process the negative files (*.n)
+            Params:
+                patttern: regex pattern to parse a line
+                target:   target predicate to look
+            '''
+            super(ProcessNegatives,self).__init__(source_file_path, *args, **kargs)
+            target = kargs.get('target', 'answer')
+            pat = compile(kargs.get('pattern', '%s\s*\(\s*(\d+)\s*,\s*[^\)]+\s*\)\s*.\s*' % target))
+            self.logger.info('Reading from "%s"',source_file_path)
+            with open(source_file_path, 'r') as source:
+                dic = {}
+                for line in source:
+                    match = pat.match(line)
+                    if match:
+                        l = dic.get(match.group(1),[])
+                        l.append(line.strip())
+                        if dic.get('random', False):
+                            shuffle(l)
+                        dic[match.group(1)] = l
+                        self.logger.debug('Negatives %s...%s',str(l)[:50],str(l)[-20:])
                     else:
-                        if boxer_flag:
-                            if pred_match.group(1) == 'modeb':
-                                boxer_mode_lines.append(line.strip())
-                            elif pred_match.group(1) == 'determination':
-                                boxer_det_lines.append(line.strip())
-                        elif deptree_flag:
-                            if pred_match.group(1) == 'modeb':
-                                dtree_mode_lines.append(line.strip())
-                            elif pred_match.group(1) == 'determination':
-                                dtree_det_lines.append(line.strip())
-                        elif header_flag:
-                            header_lines.append(line.strip())
-                        else:
-                            footer_lines.append(line.strip())
-            setting_dict.update(kargs.get('set',{}))
-            self.logger.debug('Settings: %s', setting_dict)
-            settings = map(lambda x: ':- set(%s,%s).' %x, setting_dict.iteritems())
-            self.lines.append('%Header\n')
-            self.lines.extend(header_lines)
-            if 'boxer' in sources:
-                self.lines.append('%%%%%%%%%')
-                self.lines.append('% Mode  %')
-                self.lines.append('% Boxer %')
-                self.lines.append('%%%%%%%%%\n')
-                self.lines.extend(boxer_mode_lines)
-            if 'deptree' in sources:
-                self.lines.append('%%%%%%%%%%%')
-                self.lines.append('%  Mode   %')
-                self.lines.append('% DepTree %')
-                self.lines.append('%%%%%%%%%%%\n')
-                self.lines.extend(dtree_mode_lines)
-            if 'boxer' in sources:
-                self.lines.append('%%%%%%%%%%%%%%%%%')
-                self.lines.append('% Determination %')
-                self.lines.append('%     Boxer     %')
-                self.lines.append('%%%%%%%%%%%%%%%%%\n')
-                self.lines.extend(boxer_det_lines)
-            if 'deptree' in sources:
-                self.lines.append('%%%%%%%%%%%%%%%%%')
-                self.lines.append('% Determination %')
-                self.lines.append('%    DepTree    %')
-                self.lines.append('%%%%%%%%%%%%%%%%%\n')
-                self.lines.extend(dtree_det_lines)
-            kb = kargs.get('kb',None)
-            if kb:
-                self.lines.append(":- consult('%s')." %kb)
-            self.lines.extend(footer_lines)
-            self.lines.append('%%%%%%%%%%%%')
-            self.lines.append('% Settings %')
-            self.lines.append('%%%%%%%%%%%%\n')
+                        self.logger.warning('Skipping line: "%s"', line.strip())
+            self.logger.info('Read "%s"',source_file_path)
+            lim = kargs.get('max', None)
+            for key in dic:
+                if lim == None:
+                    self.lines.extend(dic[key])
+                else:
+                    self.lines.extend(dic[key][:lim])
+            self.logger.info('Storing negatives')
+
+    class ProcessBase(ProcessFile):
+        def __init__(self, source_file_path, *args, **kargs):
+            '''
+            Process the negative files (*.n)
+            Params:
+                patttern: regex pattern to parse a line
+                target:   target predicate to look
+            '''
+            super(ProcessBase,self).__init__(source_file_path, *args, **kargs)
             
-            self.lines.extend(settings)
+            self.logger.info('Reading from "%s"',source_file_path)
+            sources = kargs.get('sources',[])
+            sources = map(lambda x: x.lower(), sources)
+            with open(source_file_path, 'r') as source:
+                boxer_mode_lines = []
+                boxer_det_lines  = []
+                dtree_mode_lines = []
+                dtree_det_lines  = []
+                header_lines = []
+                footer_lines = []
+                setting_dict = {}
+                boxer_flag = False
+                deptree_flag = False
+                header_flag = True
+                find_boxer = compile(r'%\s*((?:b|B)oxer)\s*.*')
+                find_dtree = compile(r'%\s*((?:d|D)ep(?:t|T)ree)\s*.*')
+                find_pred  = compile('^\s*:-\s*([^\(]+)\s*\(.*')
+                find_set  =  compile('^\s*:-\s*set\s*\((\w+)\s*,\s*(\w*)\s*\)\s*\.\s*')
+                for line in source:
+                    pred_match = find_pred.match(line)
+                    if find_boxer.match(line):
+                        boxer_flag = True
+                        deptree_flag = False
+                        header_flag = False
+                        self.logger.debug('Reading Boxer info from base')
+                    elif find_dtree.match(line):
+                        boxer_flag = False
+                        deptree_flag = True
+                        header_flag = False
+                        self.logger.debug('Reading DepTree info from base')
+                    elif pred_match:
+                        self.logger.debug('Line is a predicate: %s', line.strip())
+                        if pred_match.group(1) == 'set':
+                            #handle_set
+                            term, value = find_set.match(line).groups()
+                            self.logger.debug('Setting %s:%s', term, value)
+                            setting_dict[term] = value
+                        else:
+                            if boxer_flag:
+                                if pred_match.group(1) == 'modeb':
+                                    boxer_mode_lines.append(line.strip())
+                                elif pred_match.group(1) == 'determination':
+                                    boxer_det_lines.append(line.strip())
+                            elif deptree_flag:
+                                if pred_match.group(1) == 'modeb':
+                                    dtree_mode_lines.append(line.strip())
+                                elif pred_match.group(1) == 'determination':
+                                    dtree_det_lines.append(line.strip())
+                            elif header_flag:
+                                header_lines.append(line.strip())
+                            else:
+                                footer_lines.append(line.strip())
+                setting_dict.update(kargs.get('set',{}))
+                self.logger.debug('Settings: %s', setting_dict)
+                settings = map(lambda x: ':- set(%s,%s).' %x, setting_dict.iteritems())
+                self.lines.append('%Header\n')
+                self.lines.extend(header_lines)
+                if 'boxer' in sources:
+                    self.lines.append('%%%%%%%%%')
+                    self.lines.append('% Mode  %')
+                    self.lines.append('% Boxer %')
+                    self.lines.append('%%%%%%%%%\n')
+                    self.lines.extend(boxer_mode_lines)
+                if 'deptree' in sources:
+                    self.lines.append('%%%%%%%%%%%')
+                    self.lines.append('%  Mode   %')
+                    self.lines.append('% DepTree %')
+                    self.lines.append('%%%%%%%%%%%\n')
+                    self.lines.extend(dtree_mode_lines)
+                if 'boxer' in sources:
+                    self.lines.append('%%%%%%%%%%%%%%%%%')
+                    self.lines.append('% Determination %')
+                    self.lines.append('%     Boxer     %')
+                    self.lines.append('%%%%%%%%%%%%%%%%%\n')
+                    self.lines.extend(boxer_det_lines)
+                if 'deptree' in sources:
+                    self.lines.append('%%%%%%%%%%%%%%%%%')
+                    self.lines.append('% Determination %')
+                    self.lines.append('%    DepTree    %')
+                    self.lines.append('%%%%%%%%%%%%%%%%%\n')
+                    self.lines.extend(dtree_det_lines)
+                kb = kargs.get('kb',None)
+                if kb:
+                    self.lines.append(":- consult('%s')." %kb)
+                self.lines.extend(footer_lines)
+                self.lines.append('%%%%%%%%%%%%')
+                self.lines.append('% Settings %')
+                self.lines.append('%%%%%%%%%%%%\n')
+                
+                self.lines.extend(settings)
 
+    class ProcessFacts(ProcessFile):
+        def __init__(self, source_file_path, *args, **kargs):
+            '''
+            Process the fact files (*.f)
+            '''
+            super(ProcessFacts,self).__init__(source_file_path, *args, **kargs)
+            with open(source_file_path, 'r') as source:
+                for line in source:
+                    self.lines.append(line.strip())
 
-class ProcessFacts(ProcessFile):
-    def __init__(self, source_file_path, *args, **kargs):
-        '''
-        Process the fact files (*.f)
-        '''
-        super(ProcessFacts,self).__init__(source_file_path, *args, **kargs)
-        with open(source_file_path, 'r') as source:
-            for line in source:
-                self.lines.append(line.strip())
+class ProbLog:
+    '''
+        Notice that some methods inherit from the Aleph class
+    '''
+    class ProcessNegatives(Aleph.ProcessNegatives):
+        def __init__(self, source_file_path, *args, **kargs):
+            '''
+            Process the negative files (*.n)
+            Params:
+                patttern: regex pattern to parse a line
+                target:   target predicate to look
+            '''
+            if not kargs.has_key('pattern'):
+                target = kargs.get('target', 'answer')
+                kargs['pattern'] = '\d*\.?\d*\s*::\s*%s\s*\(\s*(\d+)\s*,\s*[^\)]+\s*\)\s*.\s*' % target
+            super(ProcessNegatives,self).__init__(source_file_path, *args, **kargs)
+            
+    class ProcessBase(ProcessFile):
+        def __init__(self, source_file_path, *args, **kargs):
+            '''
+            Process the negative files (*.n)
+            Params:
+                patttern: regex pattern to parse a line
+                target:   target predicate to look
+            '''
+            super(ProcessFacts,self).__init__(source_file_path, *args, **kargs)
+            with open(source_file_path, 'r') as source:
+                for line in source:
+                    self.lines.append(line.strip())
 
+    class ProcessFacts(ProcessFile):
+        def __init__(self, source_file_path, *args, **kargs):
+            '''
+            Process the fact files (*.f)
+            '''
+            super(ProcessFacts,self).__init__(source_file_path, *args, **kargs)
+            with open(source_file_path, 'r') as source:
+                for line in source:
+                    self.lines.append(line.strip())
 
 def _mkdir(dir):
     '''Makes dir if there is no dir
@@ -187,10 +224,9 @@ def _mkdir(dir):
     return True
 
 
-def write_exp(conf, kb, base, targets, negatives, folder_path,
-              copy_kb = False, prefix = 'exp', dic = None):
+def write_exp(conf, kb, base, targets, negatives, folder_path, engine, copy_kb = False, prefix = 'exp', dic = None):
     '''
-        Creates a hierarquy of folders rooted in folder_path.
+        Creates a hierarchy of folders rooted in folder_path.
 
         Params:
             conf: a list of parameters (each one as a dictionary)
@@ -198,7 +234,7 @@ def write_exp(conf, kb, base, targets, negatives, folder_path,
             base: the base file (*.b)
             targets: the fact file (*.f)
             negatives: the negative file (*.n)
-            folder_path: the folder that will root the experiments
+            folder_path: an existing folder that will root the experiments
             copy_kb: copy the knowledge base file to each leaf
                      (only do this if the kb is small)
             prefix: prefix of the leaf files generated
@@ -212,9 +248,9 @@ def write_exp(conf, kb, base, targets, negatives, folder_path,
         dic = {}
     if len(conf) == 0:
         file_path = lambda x,y: path.join(folder_path, '%s.%s'%(x,y))
-        pN = ProcessNegatives(negatives, **dic.get('n', {}))
-        pB = ProcessBase(base, kb = '%s.pl'%prefix, **dic.get('b', {}))
-        pF = ProcessFacts(targets, **dic.get('f', {}))
+        pN = engine.ProcessNegatives(negatives, **dic.get('n', {}))
+        pB = engine.ProcessBase(base, kb = '%s.pl'%prefix, **dic.get('b', {}))
+        pF = engine.ProcessFacts(targets, **dic.get('f', {}))
         with open(file_path(prefix,'n'), 'w') as n_file:
             pN.dump(n_file)
         with open(file_path(prefix,'b'), 'w') as b_file:
@@ -242,6 +278,7 @@ def write_exp(conf, kb, base, targets, negatives, folder_path,
             write_exp(conf[1:], kb, base, targets, negatives, param_path,
                       copy_kb, prefix, d)
 
+
 def parse_args(argv = argv, add_logger_args = lambda x: None):
     parser = argparse.ArgumentParser(description = 'Generates or updates all the experiments based on the current state of knowledge base')
     parser.add_argument('kb', help = 'the base file')
@@ -267,19 +304,24 @@ def main(argv):
     args = parse_args(argv, add_logger_args)
     config_logger(args)
 
+    #Read config file in conf
     if not args.configuration_file:
         args.configuration_file = 'tests.conf'
     with open(args.configuration_file, 'r') as f:
         conf = json.load(f)
+    if args.engine == 'aleph':
+        engine = Aleph()
+    else:
+        engine = ProbLog()
 
-    #if not dir_name dir: make it
-    #if not engine dir: make it
+    #if not dir_name dir make it
+    #if not engine dir make it
     _mkdir(args.dir_name)
     engine_path = path.join(args.dir_name, args.engine)
     _mkdir(engine_path)
-    #Read config file in conf
 
-    write_exp(conf, args.kb, args.base, args.facts, args.negatives, engine_path, args.copy_kb)
+    write_exp(conf, args.kb, args.base, args.facts, args.negatives,
+              engine_path, engine, args.copy_kb)
 
 if __name__ == '__main__':
     try:
