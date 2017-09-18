f2193a002c870be6729b3fc428fc8b675a96676d
brenowcarvalho <brenowcarvalho@gmail.com>
2017-09-18 10:38:20 -0300

    Adding beltagy convertion of dep trees to LF

--- analysers.py
+++ analysers.py
@@ -1,12 +1,13 @@
-from requests     import post
-from subprocess   import PIPE, Popen
-from sys          import stderr
-from os           import path
-from tempfile     import TemporaryFile
-from ConfigParser import ConfigParser
-from fol          import FOL
-from logicalform  import LF
-from regex        import match, compile
+from requests               import post
+from subprocess             import PIPE, Popen
+from sys                    import stderr
+from os                     import path
+from tempfile               import TemporaryFile
+from ConfigParser           import ConfigParser
+from fol                    import FOL
+from logicalform            import LF
+from regex                  import match, compile
+from stanford_parser.parser import Parser as StanfordParser
 import json, spacy
 import logging
 
@@ -231,8 +232,8 @@ class DependencyTreeLocalAPI:
 
     def sentence2LF(self, sentence, source = None, id = None, *args, **kargs):
         tree = self.parser(sentence.decode('utf-8'))
-        out = []
-        ids = dict([(w,'depTree%d' %(self.count+i)) for i, w in enumerate(tree)])
+        out  = []
+        ids  = dict([(w,'depTree%d' %(self.count+i)) for i, w in enumerate(tree)])
         format_text = lambda x: x.replace("'", "\\'").lower()
         self.count += len(ids)
         if not (source == None or id == None):
@@ -254,3 +255,167 @@ class DependencyTreeLocalAPI:
         #print '\n*:', text
         #print '\n>',fol
         return [LF(fol)]
+
+
+class DependencyTreeBeltagyLocalAPI:
+    _drop_tags  = ('det', 'det:predet', 'neg', 'case', 'dislocated', 'remnant', 'discourse',
+                   'aux', 'auxpass', 'cop', 'mark', 'punct', 'cc', 'cc:preconj')
+    _merge_tags = ('nmod:npmod', 'amod', 'advmod', 'compound', 'compound:prt', 'name', 'mwe',
+                   'foreign', 'goeswith', 'prt')
+    _new_tags   = ('nsubj', 'nsubjpass', 'dobj', 'iobj', 'csubj', 'csubjpass', 'ccomp',
+                   'xcomp', 'nummod', 'appos', 'nmod', 'nmod:poss', 'nmod:tmod', 'acl',
+                   'acl:relcl', 'advcl', 'list', 'parataxis', 'reparandum', 'vocative',
+                   'expl', 'conj', 'dep', 'npadvmod')
+
+    class _Dependence:
+        def __init__(self, rel, gov, dep):
+            self.dep = dep
+            self.rel = rel
+            self.gov = gov
+            self.children = []
+
+        def __str__(self):
+            return '%s -%s-> %s' %(self.dep.text, self.rel, self.gov.text)
+        def __repr__(self):
+            return self__str__()
+        def __hash__(self):
+            return (self.dep, self.rel, self.gov).__hash__()
+
+
+    def __init__(self, *args, **kargs):
+        self._init_parser(*args, **kargs)
+        self.count = 0
+        self.rel_classifier = dict()
+
+        for i in self._drop_tags:
+            self.rel_classifier[i] = self._drop
+        for i in self._merge_tags:
+            self.rel_classifier[i] = self._merge
+        for i in self._new_tags:
+            self.rel_classifier[i] = self._new
+        self.rel_classifier['ROOT'] = self._root
+
+    def _init_parser(self, *args, **kargs):
+        self.name = 'depTreeUD'
+        self.parser = StanfordParser()
+
+    def _parse_sentence_tree(self, sentence):
+        parsing = self.parser.parseToStanfordDependencies(sentence.decode('utf-8'))
+        dependencies = parsing.dependencies
+        tree = dict()
+
+        for rel,gov,dep in dependencies:
+            struct_dep = tree.get(dep, None)
+            if struct_dep == None:
+                struct_dep = self._Dependence(rel,gov,dep)
+                tree[dep] = struct_dep
+            else: #if there is a entry in the dict, update it or it might be seen as root
+                struct_dep.gov = gov
+                struct_dep.rel = rel
+            parent = tree.get(gov, self._Dependence('ROOT',gov,gov)) #if there is not a gov representation, create it
+            parent.children.append(struct_dep)
+            tree[gov] = parent
+
+        #for i in tree:
+        #    print tree[i]
+
+        for dep in tree:
+            if tree[dep].rel == 'ROOT':
+                return tree[dep]
+        return None
+
+
+    def _node2text(self,node):
+        return node.text
+
+
+    def _new_variable(self):
+        self.count += 1
+        return 'depTreeB%d' % self.count
+
+
+    def _drop(self, dep, rel, gov, variables_map):
+        return []
+
+
+    def _merge(self, dep, rel, gov, variables_map):
+        #print variables_map
+        u = variables_map[gov]
+        variables_map[dep] = u
+        return ['%s(%s)' %(self._node2text(dep), u)]
+
+
+    def _new(self, dep, rel, gov, variables_map):
+        u = variables_map[gov]
+        v = self._new_variable()
+        variables_map[dep] = v
+        return ['%s(%s)' %(self._node2text(dep), u),
+                '%s(%s,%s)' %(rel, u, v)]
+
+
+    def _root(self, dep, rel, gov, variables_map):
+        u = self._new_variable()
+        variables_map[dep] = u
+        return ['sentence_root(%s)' %u,
+                '%s(%s)' %(self._node2text(gov), u)]
+
+
+    def sentence2LF(self, sentence, source = None, id = None, *args, **kargs):
+        root = self._parse_sentence_tree(sentence)
+        out  = list()
+        variables_map = dict()
+        frontier = [root]
+
+        while(len(frontier) > 0):
+            child = frontier.pop()
+            try:
+                decision = self.rel_classifier[child.rel]
+            except KeyError:
+                raise Exception("'%s' is not a recognized tag" %child.dep_)
+            child_parsed = decision(child.dep,
+                                    child.rel,
+                                    child.gov,
+                                    variables_map)
+            out.extend(child_parsed)
+            frontier.extend(child.children)
+        out = (",".join(out)).encode("utf-8")
+        text = '%s(%s).' %(FOL.AND, out)
+        fol = FOL(text)
+        return [LF(fol)]
+
+
+class DependencyTreeBeltagySpaCyLocalAPI(DependencyTreeBeltagySpaCyLocalAPI):
+    #TODO add specific tag lists
+    def _init_parser(self, model = config.get('syntatic_local', 'spacy_model'), *args, **kargs):
+        self.name = 'depTreeBeltagySpacy'
+        self.parser = spacy.load(model)
+
+    def _parse_sentence_tree(self, sentence):
+        tree = self.parser(sentence.decode('utf-8'))
+        parsing = self.parser.parseToStanfordDependencies(sentence.decode('utf-8'))
+        dependencies = self.parser(sentence.decode('utf-8'))
+        tree = dict()
+
+        for node in dependencies:
+            gov = node.head
+            rel = node.dep_
+            dep = node
+            struct_dep = tree.get(dep, None)
+            if struct_dep == None:
+                struct_dep = self._Dependence(rel,gov,dep)
+                tree[dep] = struct_dep
+            else: #if there is an entry in the dict, update it or it might be seen as root
+                struct_dep.gov = gov
+                struct_dep.rel = rel
+            parent = tree.get(gov, self._Dependence('ROOT',gov,gov)) #if there is not a gov representation, create it
+            parent.children.append(struct_dep)
+            tree[gov] = parent
+
+        for dep in tree:
+            if tree[dep].rel == 'ROOT':
+                return tree[dep]
+        return None
+
+
+    def _node2text(self,node):
+        return str(node)
