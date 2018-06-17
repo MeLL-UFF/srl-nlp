0194a1aa4d6dcafc5dbdf2f3d6b30089e87705ac
brenowcarvalho <brenowcarvalho@gmail.com>
2018-06-17 14:57:25 -0300

    Cleanup and end-to-end construction of xml annotation file

--- eval_pipeline.py
+++ eval_pipeline.py
@@ -3,14 +3,17 @@
 """
 
 """
-import argparse
+
+import logging
+from ConfigParser import ConfigParser
+from os import path
 from sys import argv as _argv
 
-from analysers.boxer import BoxerLocalAPI
-from framenet.adapter import PARSERS_AVAILABLE
-from framenet.corpus import Paragraph, Document, Sentence
-from fsparsing import PrologAnnotator
-from logger_config import add_logger_args as _add_logger_args, config_logger
+from srl_nlp.analysers.boxer import BoxerLocalAPI
+from srl_nlp.framenet.adapter import PARSERS_AVAILABLE
+from srl_nlp.framenet.corpus import Paragraph, Document, Sentence
+from srl_nlp.fsparsing import PrologBoxerAnnotator
+from srl_nlp.logicalform import LF
 
 logger = logging.getLogger(__name__)
 
@@ -20,93 +23,167 @@ _package_directory = path.dirname(__file__)
 config.read(path.join(_package_directory, "external.conf"))
 
 
-def parse_args(argv, add_logger_args=lambda x: None):
-    parser = argparse.ArgumentParser(description='Evaluates the pipeline')
-    # parser.add_argument('dir_path', help = 'the path of the experiments')
-    parser.add_argument('data_base_path',
-                        help='path to the semeval dataset folder')
+def get_annotations(sentence, lf, var2pos):
+    # type: (Sentence, LF, dict[str,str]) -> tuple[list, list]
+    """
+        Get the annotations from the corpus sentence
 
-    # TODO implement frame matching skip
-    # parser.add_argument('-s', '--skip_frame_matching',
-    #                     action='store_true', default=False,
-    #                     help='skip the frame matching step and use the one from the data')
-    parser.add_argument('-a', '--annotation_output_path',
-                        help='output file of the annotations')
+    Args:
+        sentence:
+        lf:
+        var2pos:
 
-    parser.add_argument('-i', '--input_format',
-                        choices=PARSERS_AVAILABLE.keys(),
-                        default=PARSERS_AVAILABLE.keys()[-1],
-                        help='input annotation file format')
-    parser.add_argument('-o', '--output_format',
-                        choices=PARSERS_AVAILABLE.keys(),
-                        default=PARSERS_AVAILABLE.keys()[-1],
-                        help='output annotation file format')
-    parser.add_argument('-K', '--kb_path', default='.',
-                        help='path to knowledge base files')
-    parser.add_argument('-E', '--kb_fe', default='kb_fe',
-                        help='relative path to frame element knowledge base')
-    parser.add_argument('-R', '--kb_fr', default='kb_fr',
-                        help='relative path to path to frame matching knowledge base')
-    add_logger_args(parser)
-    args = parser.parse_args(argv[1:])
-    return args
+    Returns:
+        Two lists.
+        The first list contains the frame matching, the second one are the fe matching
+    """
+    logger.debug("Sentence: {sent}, {lf}".format(sent=sentence, lf=lf))
+    fr = []
+    fe = []
+    inv_mapping = {v: k for (k, v_list) in var2pos.items() for v in v_list}
+    for anno_set in sentence:
+        for layer in anno_set:
+            for anno in layer:
+                var_list = [inv_mapping[(start, end)] for (start, end) in inv_mapping if start >= anno.start and end <= anno.end]
+                frame_name = anno_set.frameName.lower()
+                if layer.name == 'Target':
+                    for var in var_list:
+                        fr.append(LF('frame_related({X}, {F}).'.format(X=var, F=frame_name)))
+                elif layer.name == 'FE':
+                    for var in var_list:
+                        name = anno.name.lower()
+                        fe.append(LF('frame_element({X}, {FE}, {F}).'.format(X=var, FE=name, F=frame_name)))
+    return fr, fe
 
 
-@timeit
-def main(argv):
-    args = parse_args(argv, _add_logger_args)
-    config_logger(args)
+def remove_fe_equal_to_f_from_sentence(sentence):
+    # type: (Sentence) -> None
+    fr_pos_list = [(anno.start, anno.end)
+                   for anno_set in sentence
+                   for layer in anno_set
+                   for anno in layer
+                   if layer.name == 'Target']
+    for anno_set in sentence:
+        for layer in anno_set:
+            if layer.name == 'FE':
+                layer.annotations = [anno for anno in layer if not (anno.start, anno.end) in fr_pos_list]
+        anno_set.layers = [layer for layer in anno_set if len(layer) > 0]
 
-    logger.info('Initialization')
 
-    boxer = BoxerLocalAPI()
-
-    input_parser = PARSERS_AVAILABLE[args.input_format]()
-    output_parser = PARSERS_AVAILABLE[args.output_format]()
-
-    kb_fr_path = path.join(args.kb_path, args.kb_fr)
-    kb_fe_path = path.join(args.kb_path, args.kb_fe)
-
-    annotator = PrologAnnotator(boxer, kb_fr_path, kb_fe_path)
-
-    logger.info('{anno} is parsing {file}'.format(anno=annotator, file=args.data_base_path))
-
-    with open(args.data_base_path) as db_file:
-        docs = input_parser.parseXML(db_file)
-    logger.info('Done parsing')
+def eval_corpus(annotator, boxer, docs, skip_frame_matching=False):
     out_docs = []
-
+    logger.info("Reading docs")
     for doc in docs:
-        out_doc = Document(id=doc.id, corpus_id=doc.corpusID, name=doc.name, desc=doc.desc)
+        out_doc = Document(doc_id=doc.id, corpus_id=doc.corpusID, name=doc.name, desc=doc.desc)
+
         for paragraph in doc:
-            out_paragraph = Paragraph(id=paragraph.id)
+            logger.info("Reading paragraph id({par_id})".format(par_id=paragraph.id))
+            out_paragraph = Paragraph(paragraph_id=paragraph.id)
 
             for sentence in paragraph:
-                out_sentence = Sentence(id=sentence.id, text=sentence.text, parts_of_speech=sentence.parts_of_speech)
+                logger.info("Reading sentence id({par_id}:{sent_id})"
+                            .format(par_id=paragraph.id, sent_id=sentence.id))
+                out_sentence = Sentence(sent_id=sentence.id, text=sentence.text, parts_of_speech=sentence.parts_of_speech)
+                var2pos = annotator.get_matching_variables(sentence.text)
+
                 try:
-                    matching, err = annotator.matching(sentence.text, out_error=True)
-                    matching = set(matching)
+                    if skip_frame_matching:
+                        frs, _ = get_annotations(sentence, boxer.sentence2LF(sentence.text), var2pos=var2pos)
+                        matching, err = annotator.frameElementMatching(sentence.text, fr_anno=frs, out_error=True)
+                        matching.extend(frs)
+                    else:
+                        matching, err = annotator.matching(sentence.text, out_error=True)
+
                     logger.debug(err)
-                    # print '\'%s\n\'' % '\n'.join(map(str, matching))
-                    for f_name, annoset in annotator.sem_annotations(sentence.text, matching).iteritems():
-                        # print "Frame:{f_name}\n\t{anno}".format(f_name=f_name, anno=input_parser._anno_set2XML(annoset))
+                    matching = set(matching)
+
+                    for f_name, annoset in annotator.sem_annotations(sentence.text, anno_lfs=matching, token2pos=var2pos).iteritems():
                         out_sentence.annotation_sets.append(annoset)
+
+                    remove_fe_equal_to_f_from_sentence(out_sentence)
                     out_paragraph.sentences.append(out_sentence)
-                except (AssertionError, IOError) as e:
-                    logger.error(e.message)
+
+                except (AssertionError, IOError) as ex:
+                    logger.error(ex.message)
 
             out_doc.elements.append(out_paragraph)
         out_docs.append(out_doc)
-
-    if args.annotation_output_path:
-        with open(args.annotation_output_path, 'w') as f_out:
-            output_parser.doc2XML(out_docs[0], f_out)
-    else:
-        output_parser.doc2XML(out_docs[0])
-    logger.info('Done')
+    return out_docs
 
 
 if __name__ == '__main__':
+    import argparse
+    from logger_config import add_logger_args as _add_logger_args, config_logger, timeit
+
+
+    def parse_args(argv, add_logger_args=lambda x: None):
+        parser = argparse.ArgumentParser(description='Evaluates the pipeline')
+        parser.add_argument('data_base_path',
+                            help='path to the semeval dataset folder')
+
+        parser.add_argument('-s', '--skip_frame_matching',
+                            action='store_true', default=False,
+                            help='skip the frame matching step and use the one from the data')
+        parser.add_argument('-a', '--annotation_output_path',
+                            help='output file of the annotations')
+
+        parser.add_argument('-i', '--input_format',
+                            choices=PARSERS_AVAILABLE.keys(),
+                            default=PARSERS_AVAILABLE.keys()[-1],
+                            help='input annotation file format')
+        parser.add_argument('-o', '--output_format',
+                            choices=PARSERS_AVAILABLE.keys(),
+                            default=PARSERS_AVAILABLE.keys()[-1],
+                            help='output annotation file format')
+        parser.add_argument('-K', '--kb_path', default='.',
+                            help='path to knowledge base files')
+        parser.add_argument('-E', '--kb_fe', default='kb_fe',
+                            help='relative path to frame element knowledge base')
+        parser.add_argument('-R', '--kb_fr', default='kb_fr',
+                            help='relative path to path to frame matching knowledge base')
+        add_logger_args(parser)
+        args = parser.parse_args(argv[1:])
+        return args
+
+
+    @timeit
+    def main(argv):
+        args = parse_args(argv, _add_logger_args)
+        config_logger(args)
+
+        logger.info('Initialization')
+
+        boxer = BoxerLocalAPI()
+
+        # Select the appropriate document parsers for the input and output
+        input_parser = PARSERS_AVAILABLE[args.input_format]()
+        output_parser = PARSERS_AVAILABLE[args.output_format]()
+
+        # Get the paths of the rules
+        kb_fr_path = path.join(args.kb_path, args.kb_fr)
+        kb_fe_path = path.join(args.kb_path, args.kb_fe)
+
+        annotator = PrologBoxerAnnotator(boxer, kb_fr_path, kb_fe_path)
+
+        logger.info('{anno} is parsing {file}'.format(anno=annotator, file=args.data_base_path))
+
+        # Parse corpus
+        with open(args.data_base_path) as db_file:
+            docs = input_parser.parseXML(db_file)
+        logger.info('Done parsing')
+
+        out_docs = eval_corpus(annotator, boxer, docs, skip_frame_matching=args.skip_frame_matching)
+
+        if args.annotation_output_path:
+            logger.info("Storing results at {f_name}".format(f_name=args.annotation_output_path))
+            with open(args.annotation_output_path, 'w') as f_out:
+                output_parser.doc2XML(out_docs[0], f_out)
+        else:
+            logger.info("Printing results to stdout")
+            output_parser.doc2XML(out_docs[0])
+        logger.info('Done')
+
+
     try:
         main(_argv)
     except KeyboardInterrupt:
--- fsparsing.py
+++ fsparsing.py
@@ -4,7 +4,6 @@ Script for running the Frame Semantic Parsing given some base knowledge
 """
 
 import argparse
-import spacy
 import logging
 from ConfigParser import ConfigParser
 from abc import abstractmethod
@@ -12,7 +11,9 @@ from os import path
 from sys import argv as _argv
 from tempfile import NamedTemporaryFile
 
+import spacy
 # from framenet.adapter import SemEval07XMLAdapter
+from fol import FOL
 from logger_config import timeit, config_logger, add_logger_args as _add_logger_args
 from logicalform import LF
 from rule_utils import get_factors
@@ -29,9 +30,6 @@ config.read(path.join(_package_directory, "external.conf"))
 
 class SemanticAnnotator(object):
 
-    def __init__(self, **params):
-        pass
-
     @abstractmethod
     def frameMatching(self, sentence, **params):
         pass
@@ -45,27 +43,18 @@ class SemanticAnnotator(object):
         pass
 
     @abstractmethod
-    def sem_annotations(self, sentence, lfs, **kwargs):
-        """
-        
-        Args:
-            sentence: 
-            lfs: lfs with annotations to be matched and processed
-            **kwargs: 
-
-        Returns:
-
-        """  # TODO finish doc
+    def sem_annotations(self, sentence, sentence_lfs, anno_lfs, **kwargs):
+        # type: (str, LF, list[LF], dict) -> dict[str,AnnotationSet]
         pass
 
 
-class PrologAnnotator(SemanticAnnotator, Process):
-    """Description of annotator 1"""
+class PrologBoxerAnnotator(SemanticAnnotator, Process):
+    """PrologAnnotator runs a Prolog inference over the given sentence that has been processed by the analyser"""
     FRAME_RELATED_PRED = 'frame_related'
     FRAME_ELEMENT_PRED = 'frame_element'
 
-    def __init__(self, analyser, fr_kb_file, fe_kb_file, path_to_prolog=config.get('prolog_local', 'engine')):
-        self.analyser = analyser
+    def __init__(self, boxer, fr_kb_file, fe_kb_file, path_to_prolog=config.get('prolog_local', 'engine')):
+        self.analyser = boxer
         self.fr_kb_file = fr_kb_file
         self.fe_kb_file = fe_kb_file
         self._nlp = spacy.load(config.get('syntatic_local', 'spacy_model'))  # TODO make it an argument later
@@ -144,7 +133,7 @@ class PrologAnnotator(SemanticAnnotator, Process):
         f_set_map = dict()
 
         for inf_lf in infered_lfs:
-            if inf_lf.get_pred() == PrologAnnotator.FRAME_RELATED_PRED:
+            if inf_lf.get_pred() == PrologBoxerAnnotator.FRAME_RELATED_PRED:
                 try:
                     link_term, frame_name, fe_name = inf_lf.iterterms()
                     assert link_term.isleaf() and frame_name.isleaf() and fe_name.isleaf()
@@ -162,7 +151,7 @@ class PrologAnnotator(SemanticAnnotator, Process):
 
                     # logger.error('Token \'{token}\' not found in sentence \'{sent}\''.format(sent = sentence, token = token))
 
-            if inf_lf.get_pred() == PrologAnnotator.FRAME_ELEMENT_PRED:
+            if inf_lf.get_pred() == PrologBoxerAnnotator.FRAME_ELEMENT_PRED:
                 try:
                     link_term, frame_name = inf_lf.iterterms()
                 except ValueError as ex:  # Not correct number of predicates
@@ -177,23 +166,23 @@ class PrologAnnotator(SemanticAnnotator, Process):
                     f_set_map.setdefault(frame_name,
                                          AnnotationSet(0, frame_name=frame_name, status='auto')).layers.append(layer)
 
-        return Sentence(id=0, text=sentence, annotation_sets=list(f_set_map.items()))
+        return Sentence(sent_id=0, text=sentence, annotation_sets=list(f_set_map.items()))
 
-    def parsing(self, script, sentence, header='', footer='', input_file=None):
+    def store_lf(self, sentence, input_file, footer='', header=''):
+        # type: (str, file, str, str) -> None
         lfs = self.analyser.sentence2LF(sentence)
         input_file.write(header)
         for lf in lfs:
             for pred in lf.split():
-                logger.debug('PRED: {}'.format(str(pred)))
-                input_file.write(str(pred) + '\n')
+                if pred.get_pred != FOL.OR:
+                    logger.debug('PRED: {}'.format(str(pred)))
+                    input_file.write(str(pred) + '\n')
         input_file.write(footer)
         input_file.flush()
         input_file.seek(0)
-        logger.debug('\n"Parsing:\n{}"\n'.format(script))
-        out, err = self._process(script)
-        return out, err
 
     def frameMatching(self, sentence, out_error=False, lf_file_name=None):
+        # type: (str, bool, bool) -> (str, str)
         """
 
         Args:
@@ -209,76 +198,87 @@ class PrologAnnotator(SemanticAnnotator, Process):
         with self._open_a_file(lf_file_name) as lf_file:
             script = self._script(self._load_file(self.fr_kb_file),
                                   self._load_file(lf_file.name),
-                                  self._forall(PrologAnnotator.FRAME_RELATED_PRED, 2),
+                                  self._forall(PrologBoxerAnnotator.FRAME_RELATED_PRED, 2),
                                   self._halt())
-            out, err = self.parsing(script, sentence, header='', input_file=lf_file)
+            self.store_lf(sentence, input_file=lf_file)
+            out, err = self._process(script)
             if out_error:
                 return out, err
             else:
                 return out
 
-    def frameElementMatching(self, sentence, fr_anno=tuple(), out_error=False,
-                             lf_file_name=None, **params):
+    def frameElementMatching(self, sentence, fr_anno=tuple(), lf_file_name=None, **params):
+        # type: (str, list, bool, str, any) -> tuple[str,str]
         with self._open_a_file(lf_file_name) as lf_file:
             script = self._script(self._load_file(self.fe_kb_file),
                                   self._load_file(lf_file.name),
-                                  self._forall(PrologAnnotator.FRAME_ELEMENT_PRED, 3),
+                                  self._forall(PrologBoxerAnnotator.FRAME_ELEMENT_PRED, 3),
                                   self._halt())
-            out, err = self.parsing(script, sentence, header='\n'.join(fr_anno) + '\n', input_file=lf_file)
-            if out_error:
-                return out, err
-            else:
-                return out
+            self.store_lf(sentence, header='\n'.join(map(str, fr_anno)) + '\n', input_file=lf_file)
+            out, err = self._process(script)
 
-    def matching(self, sentence, out_error=False, lf_file_name=None, **params):
+            return out, err
+
+    def matching(self, sentence, lf_file_name=None, **params):
         with self._open_a_file(lf_file_name) as lf_file:
             script = self._script(self._load_file(self.fr_kb_file),
                                   self._load_file(self.fe_kb_file),
                                   self._load_file(lf_file.name),
-                                  self._forall(PrologAnnotator.FRAME_RELATED_PRED, 2),
-                                  self._forall(PrologAnnotator.FRAME_ELEMENT_PRED, 3),
+                                  self._forall(PrologBoxerAnnotator.FRAME_RELATED_PRED, 2),
+                                  self._forall(PrologBoxerAnnotator.FRAME_ELEMENT_PRED, 3),
                                   self._halt())
-            out, err = self.parsing(script, sentence, input_file=lf_file)
-            if out_error:
-                return out, err
-            else:
-                return out
+            self.store_lf(sentence, input_file=lf_file)
+            out, err = self._process(script)
+        return out, err
 
-    def sem_annotations(self, sentence, lfs, **kwargs):
+    def sem_annotations(self, sentence, sentence_lfs=None, anno_lfs=None, var2pos_list=None, **kwargs):
+        """Gets the annotations in f
+
+            Args:
+                sentence: sentence that is going to be matched
+                sentence_lfs: list of logical form representations of the sentence
+                anno_lfs: predicate list
+                var2pos_list: dictionary mapping variable string to list of tuples (start,end)
+                **kwargs: Other parameter for compatibility. Ignored.
+
+            Returns:
+                A dictionary of frames names and annotation sets
+        """
+
         # TODO change frame_element(Var, FE, Frame) to frame_element(Var, FE, Var_frame)
         # TODO implement the interval closing
         # Get dictionaries to perform matching
-        matching = self.analyser.get_matching_tokens(sentence, output="pos")
-        sentence_lfs = self.analyser.sentence2LF(sentence)
-        matching1 = dict()
-        count = 0
-        for lf in sentence_lfs:
-            for pred in lf.split():
-                for child in pred.iterterms():
-                    if child.get_pred() in matching:
-                        matching1[pred.iterterms().next()] = matching[child.get_pred()]
-                        break
+        if not anno_lfs:
+            return dict()
+        if not sentence_lfs:
+            sentence_lfs = self.analyser.sentence2LF(sentence)
+        if var2pos_list is None:
+            token2pos = self.analyser.get_matching_tokens(sentence, 'pos')
+            var2pos_list = self.get_matching_variables(sentence, sentence_lfs, token2pos)
         # Run through lfs to get the info
         frs = dict()
-        for lf in lfs:
+        count = 0
+        for lf in anno_lfs:
             for pred in lf.split():
                 for child in pred.iterterms():
-                    if child in matching1:
+                    child_pred = child.get_pred()
+                    if child_pred in var2pos_list:
                         if pred.get_pred() == 'frame_related':
                             frame_name = pred.info[-1][0]
-                            frame = frs.get(frame_name, AnnotationSet(id="%07d" % count, frame_name=frame_name, status="Breno"))
+                            frame = frs.get(frame_name, AnnotationSet(anno_set_id="%07d" % count, frame_name=frame_name, status="Breno"))
                             layer = Layer(name="Target")
-                            start, end = matching1[child]
-                            layer.annotations.append(Annotation(name="Target", start=start, end=end))
+                            for start, end in var2pos_list[child_pred]:
+                                layer.annotations.append(Annotation(name="Target", start=start, end=end))
                             frame.layers.append(layer)
                             frs[frame_name] = frame
                             count = count + 1
                         break
 
-        for lf in lfs:
+        for lf in anno_lfs:
             for pred in lf.split():
                 for child in pred.iterterms():
-                    if child in matching1:
+                    child_pred = child.get_pred()
+                    if child_pred in var2pos_list:
                         if pred.get_pred() == 'frame_element':
                             frame_name = pred.info[3][0]
                             fe_name = pred.info[2][0]
@@ -290,14 +290,32 @@ class PrologAnnotator(SemanticAnnotator, Process):
                                 else:
                                     layer = Layer(name="FE")
                                     frame.layers.append(layer)
-                                start, end = matching1[child]
-                                layer.annotations.append(Annotation(start=start, end=end, name=fe_name))
+                                for start, end in var2pos_list[child_pred]:
+                                    layer.annotations.append(Annotation(start=start, end=end, name=fe_name))
                             else:
                                 # TODO better handle this exception
                                 # raise Exception("Oh fork, this frame element ({fe}) is orphan.".format(fe=fe_name))
                                 logger.error("Oh fork, this frame element ({fe}) is orphan.".format(fe=fe_name))
                                 break
         return frs
+
+    def get_matching_variables(self, sentence, sentence_lfs=None, matching=None):
+        # type: (str, list[LF],dict[any,any]) -> dict[LF,list[any]]
+        if not sentence_lfs:
+            sentence_lfs = self.analyser.sentence2LF(sentence)
+        if not matching:
+            matching = self.analyser.get_matching_tokens(sentence, output="pos")
+        matching1 = dict()
+        for lf in sentence_lfs:
+            for pred in lf.split():
+                for child in pred.iterterms():
+                    if child.get_pred() in matching:
+                        term = pred.iterterms().next().get_pred()
+                        tmp_list = matching1.get(term, [])
+                        tmp_list.append(matching[child.get_pred()])
+                        matching1[term] = tmp_list
+                        break
+        return matching1
 
 
 def parse_args(argv, add_logger_args=lambda x: None):
@@ -344,7 +362,7 @@ def main(argv):
     boxer = BoxerLocalAPI()
     kb_fr_path = path.join(args.kb_path, args.kb_fr)
     kb_fe_path = path.join(args.kb_path, args.kb_fe)
-    anno = PrologAnnotator(boxer, kb_fr_path, kb_fe_path)
+    anno = PrologBoxerAnnotator(boxer, kb_fr_path, kb_fe_path)
 
     print 'LF: %s' % boxer.sentence2LF(args.sentence)
 
@@ -362,7 +380,7 @@ def main(argv):
 
     if args.matching:
         print '\nMatching:'
-        out, err = anno.matching(args.sentence, out_error=True, lf_file_name=args.tmp_lf_file)
+        out, err = anno.matching(args.sentence, lf_file_name=args.tmp_lf_file)
         logger.debug(err)
         print '\'%s\n\'' % '\n'.join(map(str, out))
         # semeval_parser = SemEval07XMLAdapter()
